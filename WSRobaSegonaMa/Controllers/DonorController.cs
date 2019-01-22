c1cac83849f82b8782dcb15c9fc3cc464b7ce3d4
VAuyanet <victor.auyanet@gmail.com>
2019-01-22 20:17:35 +0100

    Base de WS, conectat a BDD

---
+++ WSRobaSegonaMa/Controllers/DonorController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WSRobaSegonaMa.Models;
+
+namespace WSRobaSegonaMa.Controllers
+{
+    public class DonorController : ApiController
+    {
+        // GET: api/emails
+        [Route("api/emails")]
+        public HttpResponseMessage Get()
+        {
+            var contactes = DonorRepository.GetAllDonors();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, contactes);
+            return response;
+        }
+        // GET: api/emailsTot
+        [Route("api/emailsTot")]
+        public HttpResponseMessage GetTot()
+        {
+            var telefons = DonorRepository.GetAllDonors();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, telefons);
+            return response;
+        }
+
+        [Route("api/emailC/{dni:alpha}")]
+        public HttpResponseMessage GetEmailByName(string dni)
+        {
+            var telefons = DonorRepository.SearchDonorsByDni(dni);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, telefons);
+            return response;
+        }
+
+        // GET: api/email/5
+        [Route("api/email/{id?}")]
+        public HttpResponseMessage Get(int id)
+        {
+            var telefon = DonorRepository.GetDonor(id);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, telefon);
+            return response;
+        }
+
+        // PUT: api/email/5
+        [Route("api/email/{id?}")]
+        public HttpResponseMessage Put(int id, [FromBody] Donor val)
+        {
+            var telefon = DonorRepository.UpdateDonor(id, val);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, telefon);
+            return response;
+        }
+
+        // POST: api/email
+        [Route("api/email")]
+        public HttpResponseMessage Post([FromBody] Donor val)
+        {
+            var email = DonorRepository.InsertDonor(val);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, email);
+            return response;
+        }
+
+
+        // DELETE: api/email/5
+        [Route("api/email/{id?}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            DonorRepository.DeleteDonor(id);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "S'ha esborrat correctament");
+            return response;
+        }
+    }
+}
--- WSRobaSegonaMa/Models/DonorRepository.cs
+++ WSRobaSegonaMa/Models/DonorRepository.cs
@@ -7,5 +7,79 @@ namespace WSRobaSegonaMa.Models
 {
     public class DonorRepository
     {
+
+        private static RobaSegonaMaEntities dataContext = new RobaSegonaMaEntities();
+
+        public static List<Donor> GetAllDonors()
+        {
+            List<Donor> lc = dataContext.Donors.ToList();
+            return lc;
+        }
+
+        public static Donor GetDonor(int donorID)
+        {
+            Donor c = dataContext.Donors.Where(x => x.Id == donorID).SingleOrDefault();
+            return c;
+        }
+
+        public static List<Donor> SearchDonorsByDni(string donorDni)
+        {
+            List<Donor> lc = dataContext.Donors
+                .Where(x => x.dni.Contains(donorDni)).ToList();
+            return lc;
+        }
+
+        public static Donor InsertDonor(Donor c)
+        {
+            try
+            {
+                dataContext.Donors.Add(c);
+                dataContext.SaveChanges();
+                return GetDonor(c.Id);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static Donor UpdateDonor(int id, Donor c)
+        {
+            try
+            {
+                Donor c0 = dataContext.Donors.Where(x => x.Id == id).SingleOrDefault();
+                if (c.name != null) c0.name = c.name;
+                if (c.lastName != null) c0.lastName = c.lastName;
+                if (c.birthDate != null) c0.birthDate = c.birthDate;
+                if (c.gender != null) c0.gender = c.gender;
+                if (c.password != null) c0.password = c.password;
+                if (c.email != null) c0.email = c.email;
+                if (c.securityAnswer != null) c0.securityAnswer = c.securityAnswer;
+                if (c.securityQuestion != null) c0.securityQuestion = c.securityQuestion;
+                if (c.dateCreated != null) c0.dateCreated = c.dateCreated;
+                if (c.active != null) c0.active = c.active;
+                if (c.picturePath != null) c0.picturePath = c.picturePath;
+                if (c.ammountGiven != null) c0.ammountGiven = c.ammountGiven;
+                if (c.Language != null) c0.Language = c.Language;
+                if (c.dni != null) c0.dni = c.dni;
+
+                dataContext.SaveChanges();
+                return GetDonor(id);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static void DeleteDonor(int id)
+        {
+            Donor c = dataContext.Donors.Where(x => x.Id == id).SingleOrDefault();
+            if (c != null)
+            {
+                dataContext.Donors.Remove(c);
+                dataContext.SaveChanges();
+            }
+        }
     }
 }
