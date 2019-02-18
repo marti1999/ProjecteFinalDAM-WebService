23cbd1a08c5f34445c2838d65014239f0400180c
marti <marti@DESKTOP-65875CT>
2019-02-18 19:15:45 +0100

    colors repository and controllers

---
+++ WSRobaSegonaMa/Controllers/ColorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WSRobaSegonaMa.Models;
+
+namespace WSRobaSegonaMa.Controllers
+{
+    public class ColorController : ApiController
+    {
+        // GET: api/colors
+        [Route("api/colors")]
+        public HttpResponseMessage Get()
+        {
+            var colors = ColorRepository.GetAllColors();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, colors);
+            return response;
+        }
+    }
+}
---
+++ WSRobaSegonaMa/Models/ColorRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSRobaSegonaMa.Models
+{
+    public class ColorRepository
+    {
+        private static RobaSegonaMaEntities dataContext = new RobaSegonaMaEntities();
+
+        public static List<Color> GetAllColors()
+        {
+            List<Color> lc = dataContext.Colors.ToList();
+            return lc;
+        }
+    }
+}
