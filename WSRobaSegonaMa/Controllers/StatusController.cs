d7f6412617b242078a97f626ffa1e5601aa78f31
VAuyanet <victor.auyanet@gmail.com>
2019-02-26 20:11:06 +0100

    Status Created

---
+++ WSRobaSegonaMa/Controllers/StatusController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WSRobaSegonaMa.Models;
+
+namespace WSRobaSegonaMa.Controllers
+{
+    public class StatusController : ApiController
+    {
+        // GET: api/status
+        [Route("api/status")]
+        public HttpResponseMessage Get()
+        {
+            var requestors = RequestorRepository.GetAllRequestors();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, requestors);
+            return response;
+        }
+
+        // PUT: api/status/5
+        [Route("api/status/{id?}")]
+        public HttpResponseMessage Put(int id, [FromBody] Requestor val)
+        {
+            var requestors = RequestorRepository.UpdateRequestor(id, val);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, requestors);
+            return response;
+        }
+
+        // POST: api/status
+        [Route("api/status")]
+        public HttpResponseMessage Post([FromBody] Requestor val)
+        {
+            var requestors = RequestorRepository.InsertRequestor(val);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, requestors);
+            return response;
+        }
+
+
+        // DELETE: api/status/5
+        [Route("api/status/{id?}")]
+        public HttpResponseMessage Delete(String id)
+        {
+            RequestorRepository.DeleteRequestor(id);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+    }
+}
---
+++ WSRobaSegonaMa/Models/StatusRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WSRobaSegonaMa.Controllers;
+
+
+namespace WSRobaSegonaMa.Models
+{
+    public class StatusRepository
+    {
+
+        private static RobaSegonaMaEntities dataContext = new RobaSegonaMaEntities();
+
+        public static List<Status> GetAllStatus()
+        {
+            List<Status> lc = dataContext.Status.ToList();
+            return lc;
+        }
+
+        public static Status GetStatus(int requestorID)
+        {
+            Status c = dataContext.Status.Where(x => x.Id == requestorID).SingleOrDefault();
+            return c;
+        }
+
+        public static Status InsertStatus(Status c)
+        {
+            try
+            {
+                dataContext.Status.Add(c);
+                dataContext.SaveChanges();
+                return GetStatus(c.Id);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static Status UpdateStatus(int id, Status c)
+        {
+            try
+            {
+                Status c0 = dataContext.Status.Where(x => x.Id == id).SingleOrDefault();
+                if (c.Id != null) c0.Id = c.Id;
+                if (c.status1 != null) c0.status1 = c.status1;
+                if (c.reason != null) c0.reason = c.reason;
+
+                dataContext.SaveChanges();
+                return GetStatus(id);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public static void DeleteStatus(int id)
+        {
+            Status c;
+
+            c = dataContext.Status.Where(x => x.Id == id).SingleOrDefault();
+
+
+            if (c != null)
+            {
+                dataContext.Status.Remove(c);
+                dataContext.SaveChanges();
+            }
+        }
+
+    }
+}
