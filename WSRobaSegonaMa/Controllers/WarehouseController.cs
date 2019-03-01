2e4bb710bb66e78af74b9c97a2470ee7814c29f8
marti <marti@DESKTOP-65875CT>
2019-03-01 18:55:40 +0100

    warehouse controller i repository

---
+++ WSRobaSegonaMa/Controllers/WarehouseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WSRobaSegonaMa.Models;
+
+namespace WSRobaSegonaMa.Controllers
+{
+    public class WarehouseController : ApiController
+    {
+        // GET: api/warehouses
+        [Route("api/warehouses")]
+        public HttpResponseMessage GetWarehouses()
+        {
+
+            var warehouse = warehouseRepository.GetAllWarehouses();
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, warehouse);
+            return response;
+        }
+         
+
+
+    }
+}
---
+++ WSRobaSegonaMa/Models/warehouseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSRobaSegonaMa.Models
+{
+    public class warehouseRepository
+    {
+        private static RobaSegonaMaEntities dc = new RobaSegonaMaEntities();
+
+        public static List<Warehouse> GetAllWarehouses()
+        {
+            List<Warehouse> lc = dc.Warehouses.ToList();
+            return lc;
+        }
+    }
+}
