28176ded0ab43ad588bd29368e9406cd6da55605
marti <marti@DESKTOP-65875CT>
2019-03-12 21:05:47 +0100

    order controller i repository

---
+++ WSRobaSegonaMa/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WSRobaSegonaMa.Models;
+
+namespace WSRobaSegonaMa.Controllers
+{
+    public class OrderController : ApiController
+    {
+        // PUT: api/order
+        [Route("api/order")]
+        public HttpResponseMessage Post([FromBody] Order val)
+        {
+            var orders = OrderRepository.insertOrder(val);
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, orders);
+            return response;
+        }
+    }
+}
