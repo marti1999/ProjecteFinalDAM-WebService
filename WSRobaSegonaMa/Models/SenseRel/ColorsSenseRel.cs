ff131cb5a633734645db0ebad3d4c02d240710d7
marti <marti@DESKTOP-65875CT>
2019-03-27 18:58:15 +0100

    posat json ignore a partials class que van amb Cloth

---
+++ WSRobaSegonaMa/Models/SenseRel/ColorsSenseRel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace WSRobaSegonaMa.Models
+{
+    public partial class Color
+
+    {
+
+        public bool ShouldSerializeClothes()
+        {
+            return serialitzable;
+        }
+
+        [JsonIgnore]
+        public bool serialitzable = false;
+    }
+}
